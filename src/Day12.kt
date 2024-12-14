b1ac050df94c36a861f7b58e7626586f87be1aa8
Erwin Wessels <erwin@pleo.io>
2024-12-14 16:11:29 +0100

    Day 12 part 2

--- src/Day12.kt
+++ src/Day12.kt
@@ -5,7 +5,14 @@ import kotlin.math.abs
 private const val DAY = "12"
 
 private const val PART1_CHECK = 1930
-private const val PART2_CHECK = 0
+private const val PART2_CHECK = 1206
+
+private enum class Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+}
 
 fun main() {
     data class Coord(
@@ -46,7 +53,7 @@ fun main() {
 
         map.forEachIndexed { y, row ->
             row.forEachIndexed { x, plant ->
-                val coord = Coord(y, x)
+                val coord = Coord(x, y)
                 val neighbouringIndexAreas =
                     areas
                         .mapIndexed { index, area -> index to area }
@@ -101,14 +108,140 @@ fun main() {
         }
     }
 
-    fun part2(input: List<String>): Int = input.size
+    data class Side(
+        val direction: Direction,
+        val coords: MutableSet<Coord>,
+    ) {
+        constructor(direction: Direction, coord: Coord) : this(direction, mutableSetOf(coord))
+    }
+
+    fun part2(input: List<String>): Int {
+        val map = input.map { it.chunked(1) }
+        val areas = mutableListOf<Area>()
+
+        map.forEachIndexed { y, row ->
+            row.forEachIndexed { x, plant ->
+                val coord = Coord(x, y)
+                val neighbouringIndexAreas =
+                    areas
+                        .mapIndexed { index, area -> index to area }
+                        .filter { (_, area) ->
+                            area.plant == plant && area.coords.any { coordsAreNeighbour(it, coord) }
+                        }
+
+                when (neighbouringIndexAreas.size) {
+                    0 -> areas.add(Area(plant, coord))
+                    1 ->
+                        neighbouringIndexAreas
+                            .first()
+                            .second.coords
+                            .add(coord)
+                    else -> {
+                        val mergingIndexes = neighbouringIndexAreas.map { it.first }.sortedDescending()
+                        mergingIndexes.forEach { indexToRemove -> areas.removeAt(indexToRemove) }
+                        val mergedArea = Area(plant, coord)
+                        mergedArea.coords.addAll(neighbouringIndexAreas.flatMap { it.second.coords })
+                        areas.add(mergedArea)
+                    }
+                }
+            }
+        }
+
+        fun Area.area(): Int = this.coords.size
+
+        fun Area.sides(): Int {
+            val sides = mutableListOf<Side>()
+            coords.forEach { coord ->
+                if (coord.north() !in this) {
+                    // have an edge - see if any of the neighbours are in a side?
+                    sides
+                        .firstOrNull { side ->
+                            side.direction == Direction.NORTH &&
+                                (coord.west() in side.coords || coord.east() in side.coords)
+                        }?.coords
+                        ?.add(coord)
+                        ?: sides.add(Side(Direction.NORTH, coord))
+                }
+                if (coord.east() !in this) {
+                    sides
+                        .firstOrNull { side ->
+                            side.direction == Direction.EAST &&
+                                (coord.north() in side.coords || coord.south() in side.coords)
+                        }?.coords
+                        ?.add(coord)
+                        ?: sides.add(Side(Direction.EAST, coord))
+                }
+                if (coord.south() !in this) {
+                    sides
+                        .firstOrNull { side ->
+                            side.direction == Direction.SOUTH &&
+                                (coord.east() in side.coords || coord.west() in side.coords)
+                        }?.coords
+                        ?.add(coord)
+                        ?: sides.add(Side(Direction.SOUTH, coord))
+                }
+                if (coord.west() !in this) {
+                    sides
+                        .firstOrNull { side ->
+                            side.direction == Direction.WEST &&
+                                (coord.north() in side.coords || coord.south() in side.coords)
+                        }?.coords
+                        ?.add(coord)
+                        ?: sides.add(Side(Direction.WEST, coord))
+                }
+            }
+            // Need to merge sides that are contiguous
+            val sidesToRemove = mutableListOf<Int>()
+            sides.indices.reversed().forEach { sideIndex ->
+                if (sideIndex == 0) {
+                    return@forEach
+                }
+                val side = sides[sideIndex]
+                val contiguousSide =
+                    sides.take(sideIndex - 1).firstOrNull { targetSide ->
+                        if (side.direction != targetSide.direction) {
+                            return@firstOrNull false
+                        }
+                        if (side.direction == Direction.NORTH || side.direction == Direction.SOUTH) {
+                            side.coords.any { sideCoord ->
+                                targetSide.coords.any { targetSideCoord ->
+                                    sideCoord.east() == targetSideCoord || sideCoord.west() == targetSideCoord
+                                }
+                            }
+                        } else {
+                            side.coords.any { sideCoord ->
+                                targetSide.coords.any { targetSideCoord ->
+                                    sideCoord.north() == targetSideCoord || sideCoord.south() == targetSideCoord
+                                }
+                            }
+                        }
+                    }
+                if (contiguousSide != null) {
+                    sidesToRemove.add(sideIndex)
+                    contiguousSide.coords.addAll(side.coords)
+                    side.coords.removeAll { true }
+                }
+            }
+            sidesToRemove.forEach { sides.removeAt(it) }
+
+            return sides.size
+        }
+
+//        areas.forEach { area ->
+//            println("Area '${area.plant}'; area ${area.area()}, sides ${area.sides()}")
+//        }
+
+        return areas.sumOf { area ->
+            area.area() * area.sides()
+        }
+    }
 
     println("Day $DAY")
     val testInput = readInput("Day${DAY}_test")
     check(part1(testInput).also { println("Part1 output: $it") } == PART1_CHECK)
-//    check(part2(testInput).also { println("Part2 output: $it") } == PART2_CHECK)
+    check(part2(testInput).also { println("Part2 output: $it") } == PART2_CHECK)
 
     val input = readInput("Day$DAY")
     println("Part1 final output: ${part1(input)}")
-//    println("Part2 final output: ${part2(input)}")
+    println("Part2 final output: ${part2(input)}")
 }
